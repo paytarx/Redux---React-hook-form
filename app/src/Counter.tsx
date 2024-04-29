3f351171c56ee43bbbb82ef66d65e842e4bcddf1
paytarx <oz.556@hotmail.com>
2024-04-29 17:46:28 +0300

    style add

--- app/src/Counter.tsx
+++ app/src/Counter.tsx
@@ -15,34 +15,42 @@ const Counter = () => {
 
     return (
     <div>
-          <div>
+          <div style={{marginLeft: "20px"}}>
             {count}
+            {
+                count === 35 ? <span> - Default count</span> : ""
+            }
         </div>
         <div className="flex gap-x-1">
         <button
+        style={{backgroundColor: "blue", color: "white", marginTop: "15px"}}
             aria-label='Increment value'
             onClick={() => dispatch(increment())}
             >
                 increment
             </button>
             <button
+             style={{backgroundColor: "blue", color: "white", margin:"0px 10px"}}
             aria-label='Decrement value'
             onClick={() => dispatch(decrement())}
             >
                 decrement
             </button>
             <button
+             style={{backgroundColor: "blue", color: "white", margin:"0px 10px"}}
             aria-label='Decrement value'
             onClick={() => handleClickAmount(5)}
             >
                 increment by 5
             </button>
             <button
+             style={{backgroundColor: "blue", color: "white" , margin: "0px 10px"}}
             onClick={() => handleClickAmount(200)}
             >
                 increment 200
             </button>
             <button
+             style={{backgroundColor: "blue", color: "white", margin: "10px 0px"}}
             onClick={() => handleClickAmount(-100)}
             >
                 decrement 100
